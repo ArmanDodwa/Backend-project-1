c033d469158f8b8063a554f50d92008a094ee357
Arman Dodwa <arman9669324697@gmail.com>
2025-09-03 00:17:05 +0530

    Added flight model

--- src/models/airport.js
+++ src/models/airport.js
@@ -10,6 +10,21 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.City, { 
+        foreignKey: "cityId",
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE", 
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: "departureAirportId",
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: "arrivalAirportId",
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      });
     }
   }
   Airport.init(
